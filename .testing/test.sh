5fb573abf3946d62abca88bd2555ee85977638e1
Philipp Gaßner <activator112233@gmail.com>
2022-11-01 22:14:01 +0100

    bugfix

---
+++ .testing/test.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+# test p10k
+# test xplr cmd
+# verify correctnes of config files
---
+++ xplr/init.lua
@@ -0,0 +1,3 @@
+version = "0.19.0"
+xplr.config.general.show_hidden = true
+xplr.config.general.enable_mouse = true
